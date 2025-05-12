f04f345dbd6713dfc1c275d9791b7f8c0250bc98
Ibra <akundokumenibra@gmail.com>
2025-05-12 15:40:47 +0700

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,44 @@
+import Aurora from "@/components/Aurora";
+import ArrowUpRigtIcon from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: 'YouTube',
+    href: '#',
+  },
+  {
+    title: 'Github',
+    href: '#',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+  {
+    title: 'TikTok',
+    href: '#',
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="relative z-0 overflow-x-clip">
+      <div className="rotate-180 absolute inset-0 h-[400px] -mt-28 md:-mt-80 bottom-0 min-w-screen -z-10 opacity-50">
+        <Aurora />
+      </div>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
+          <div className="text-white/40">&copy; 2025. All rights reserved.</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">  
+            {footerLinks.map(link => (
+              <a href={link.href} key={link.title} className="inline-flex items-center gap-1.5">
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRigtIcon className="size-4"/>
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
